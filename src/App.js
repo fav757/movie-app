89714cb34d4c36c74cba4ad2b57cc3f7bb2a212c
fav757 <fav755@gmail.com>
2020-09-25 15:23:44 +0300

    add Hero section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <HeroSection />
+      </main>
     </div>
   );
 }
