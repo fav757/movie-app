6d3394d5ab98a745be7d427cf3d2dfd356497a07
Alex <fav757@github.com>
2020-09-25 11:50:40 +0300

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        container
+      </div>
+    </header>
+  );
+}
+
+export default Header;
