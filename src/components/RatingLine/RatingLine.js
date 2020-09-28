98b61ffe25be871f8614f36c9a801266d47067f7
fav757 <fav755@gmail.com>
2020-09-28 13:06:43 +0300

    code style improvement

--- src/components/RatingLine/RatingLine.js
+++ src/components/RatingLine/RatingLine.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './RatingLine.module.scss';
 
-function RatingLine(props) {
-  const smileIcon =
-    props.rating > 6.66 ? 'smile' : props.rating > 3.33 ? 'meh' : 'frown';
+function RatingLine({ rating }) {
+  const smileIcon = rating > 6.66 ? 'smile' : rating > 3.33 ? 'meh' : 'frown';
 
   return (
     <div className={styles.container + ' ' + smileIcon}>
       <i className={'far fa-' + smileIcon}></i>
-      <span> {props.rating}</span>
+      <span> {rating}</span>
     </div>
   );
 }
