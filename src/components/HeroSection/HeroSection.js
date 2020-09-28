ea9e5dba311430224a09c9a6bb583a71a9fccde4
fav757 <fav755@gmail.com>
2020-09-28 09:42:09 +0300

    add try catch constuction to hero seftion for handling request errors

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,40 +3,54 @@ import styles from './HeroSection.module.scss';
 import randomNumberInRange from '../RandomNubmerInRange/RandomNumberInRange';
 import ganresIdDatabase from '../GanresIdDatabase/GanresIdDatabase.json';
 
+const moviePlaceholder = {
+  title: 'Money plane',
+  release_date: '2020-08-25',
+  backdrop_path: '/pq0JSpwyT2URytdFG0euztQPAyR.jpg',
+  genre_ids: ['28', '53'],
+  overview:
+    'A hit man named Kai flies into Bangkok, gets a gun, and orders a cab.',
+};
+
 function HeroSection() {
-  const [movieData, setmovieData] = useState({});
+  const [movieData, setmovieData] = useState(moviePlaceholder);
 
   useEffect(() => {
     const randomNumber = randomNumberInRange(0, 20);
 
-    fetch(`
-    https://api.themoviedb.org/3/movie/popular?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1
-    `)
-      .then((response) => response.json())
-      .then((json) => setmovieData(json.results[randomNumber]));
+    (async function () {
+      try {
+        const request = await fetch(`
+          https://api.themoviedb.org/3/movie/popular?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1
+        `);
+        const response = await request.json();
+        setmovieData(response.results[randomNumber]);
+      } catch (e) {
+        console.log(e, "Can't recive data from server");
+      }
+    })();
   }, []);
 
   return (
     <section
       className={styles.container}
       style={{
-        backgroundImage:
-          `
+        backgroundImage: `
           linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
           url('https://image.tmdb.org/t/p/original${movieData['backdrop_path']}
-        ` || null,
+        `,
       }}
     >
       <div className={styles.wrap}>
         <div className={styles.descriptor}>
-          <h1>{movieData['title']}</h1>
+          <h1>{movieData.title}</h1>
           <b>
-            {movieData['release_date']} |{' '}
-            {(movieData['genre_ids'] || [])
+            {movieData.release_date} |
+            {movieData.genre_ids
               .map((ganre) => ganresIdDatabase[ganre])
               .join(', ')}
           </b>
-          <p>{movieData['overview']}</p>
+          <p>{movieData.overview}</p>
           <div>
             <span>Watch trailer</span>
           </div>
