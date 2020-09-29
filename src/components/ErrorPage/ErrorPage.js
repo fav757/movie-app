aed932e07c29622ac49452848e22135539a79761
fav757 <fav755@gmail.com>
2020-09-29 11:01:25 +0300

    fill the error page component

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import PopularityLine from '../PopularityLine/PopularityLine';
+import { Link } from 'react-router-dom';
 import styles from './ErrorPage.module.scss';
 
 function ErrorPage() {
   return (
-    <div>
-      <h1>Sorry, but we cant find such page on this website</h1>
+    <div className={styles.container}>
+      <div className={styles.error}>
+        <h2>Unfortunately, we cant find such page on this website.</h2>
+        <p>
+          We're sorry to inform you that we can't display the page you looked
+          for.
+        </p>
+        <p>
+          Also you continue from the <Link to='/'>main page</Link>.
+        </p>
+      </div>
     </div>
   );
 }
