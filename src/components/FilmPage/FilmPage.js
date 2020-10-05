c10125fb393e82eda9a5e0c3f8bfa4a12b818ce1
fav757 <fav755@gmail.com>
2020-10-05 14:13:15 +0300

    add missing poster placeholder

--- src/components/FilmPage/FilmPage.js
+++ src/components/FilmPage/FilmPage.js
@@ -6,11 +6,11 @@ import PostersGrid from '../PostersGrid/PostersGrid';
 function FilmPage() {
   const showType = window.location.href.match(/(?<=#)[^?]+/)[0];
   const showId = window.location.href.match(/(?<=\?id=)[0-9]+/)[0];
-  const requestLink =
-    `https://api.themoviedb.org/3/${showType}/${showId}/similar?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1`;
+  const requestLink = `https://api.themoviedb.org/3/${showType}/${showId}/similar?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1`;
+  const handleLoad = window.scrollTo({ top: 0, behavior: 'smooth' });
 
   return (
-    <div>
+    <div onLoad={handleLoad}>
       <FilmBanner showId={showId} showType={showType} />
       <CastRow filmId={showId} showType={showType} />
       <PostersGrid requestLink={requestLink} header='Simillar' />
--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -2,33 +2,27 @@ import React from 'react';
 import styles from './Poster.module.scss';
 import ganresIdDatabase from '../GanresIdDatabase/GanresIdDatabase.json';
 import { Link } from 'react-router-dom';
+import noPoster from './noPoster.png';
 
 function Poster({ data }) {
   const posterImg = 'https://image.tmdb.org/t/p/w500' + data.poster_path;
   const showType = data.first_air_date ? 'tv' : 'movie';
 
   return (
-    <Link
-      to={() => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
-        return `/${showType}?id=${data.id}`;
-      }}
-      className={styles.container}
-    >
+    <Link to={`/${showType}?id=${data.id}`} className={styles.container}>
       <i className={'fas fa-spinner ' + styles.spinner}></i>
       <img
         loading='lazy'
         className={styles.poster}
-        src={posterImg}
+        src={data.poster_path ? posterImg : noPoster}
         alt='poster'
       />
       <div className={styles.desriptionWrap}>
         <div className={styles.desription}>
           <h3>{data.title || data.name}</h3>
-          <p>{(data.release_date || data.first_air_date).slice(0, 4)}</p>
+          <p>
+            {(data.release_date || data.first_air_date || 'xxxx').slice(0, 4)}
+          </p>
           <b>
             {data.genre_ids.map((ganre) => ganresIdDatabase[ganre]).join(', ')}
           </b>
