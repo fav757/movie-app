81c8c14bcc2c465fa7e6456ff062fc44e617a8f6
fav757 <fav755@gmail.com>
2020-10-05 11:31:20 +0300

    add router links to nav bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.scss';
 
 function Navigation() {
   return (
     <ul className={styles.list}>
-      <li>Trend</li>
-      <li>New</li>
-      <li>My list</li>
+      <li>
+        <NavLink to='/tranding' activeClassName={styles.activeLink}>
+          Trending
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/movie-top' activeClassName={styles.activeLink}>
+          Top films
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/tv-top' activeClassName={styles.activeLink}>
+          Top tv
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/list' activeClassName={styles.activeLink}>
+          My list
+        </NavLink>
+      </li>
     </ul>
   );
 }
