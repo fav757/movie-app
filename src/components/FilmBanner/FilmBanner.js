7efadafa5b26a75788af8cdac0b25e152a85a3ca
fav757 <fav755@gmail.com>
2020-10-05 18:01:29 +0300

    changed film banner section fallback logic

--- src/components/FilmBanner/FilmBanner.js
+++ src/components/FilmBanner/FilmBanner.js
@@ -3,24 +3,10 @@ import styles from './FilmBanner.module.scss';
 import RatingLine from '../RatingLine/RatingLine';
 import PopularityLine from '../PopularityLine/PopularityLine';
 import FirmInfo from '../FirmInfo/FirmInfo';
-
-const initialPage = {
-  backdrop_path: `linear-gradient(to right, rgba(24, 28, 29, 1), rgba(24, 28, 29, 0.75))`,
-  poster_path: null,
-  title: 'Loading title',
-  adult: false,
-  release_date: '1337/02/28',
-  genres: [{ name: 'loading genres' }],
-  runtime: 'wait few',
-  tagline: 'You will see content as soon as possible',
-  first_air_date: '10',
-  last_air_date: '30 seconds to wait',
-  number_of_episodes: 0,
-  number_of_seasons: 0,
-};
+import noPoster from '../Poster/noPoster.png';
 
 function FilmBanner({ showId, showType }) {
-  const [details, setDeatails] = useState(initialPage);
+  const [details, setDeatails] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
   const [, setError] = useState(false);
 
@@ -31,9 +17,7 @@ function FilmBanner({ showId, showType }) {
           `https://api.themoviedb.org/3/${showType}/${showId}?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US`
         );
         const response = await request.json();
-        response.backdrop_path = `linear-gradient(to right, rgba(24, 28, 29, 1), rgba(24, 28, 29, 0.75)),
-          url('https://image.tmdb.org/t/p/original${response.backdrop_path}`;
-        response.poster_path = `https://image.tmdb.org/t/p/original${response.poster_path}`;
+
         setDeatails(response);
       } catch (e) {
         console.log(e);
@@ -47,29 +31,39 @@ function FilmBanner({ showId, showType }) {
   return (
     <div
       className={styles.container}
-      style={{ backgroundImage: details.backdrop_path }}
+      style={{
+        backgroundImage: details.backdrop_path
+          ? `linear-gradient(to right, rgba(24, 28, 29, 1), rgba(24, 28, 29, 0.75)),
+            url('https://image.tmdb.org/t/p/original${details.backdrop_path}`
+          : null,
+      }}
       data-loaded={isLoaded || null}
     >
       <div className={styles.filmPage}>
         <div className={styles.posterContainer}>
           <i className={'fas fa-spinner ' + styles.spinner}></i>
-          {details.poster_path ? (
-            <img src={details.poster_path} alt='poster' />
-          ) : null}
+          <img
+            src={
+              details.poster_path
+                ? `https://image.tmdb.org/t/p/original${details.poster_path}`
+                : noPoster
+            }
+            alt='poster'
+          />
         </div>
         <div className={styles.infoContainer}>
           <h2 className={styles.title}>
             <a
               target='_blank'
               rel='noopener noreferrer'
-              href={details.homepage}
+              href={details.homepage || '#'}
             >
-              {details.title || details.name}
+              {details.title || details.name || 'Movie title'}
             </a>
             <span>
               (
               {new Date(
-                details.release_date || details.first_air_date
+                details.release_date || details.first_air_date || new Date()
               ).getFullYear()}
               )
             </span>
@@ -86,7 +80,9 @@ function FilmBanner({ showId, showType }) {
             ) : (
               <span>{details.release_date}</span>
             )}
-            <span>{details.genres.map((ganre) => ganre.name).join(', ')}</span>
+            <span>
+              {(details.genres || []).map((ganre) => ganre.name).join(', ')}
+            </span>
             <span>
               {showType === 'tv'
                 ? details.number_of_episodes +
@@ -107,7 +103,10 @@ function FilmBanner({ showId, showType }) {
           <div className={styles.tagline}>
             {details.tagline || 'Description:'}
           </div>
-          <div>{details.overview}</div>
+          <div>
+            {details.overview ||
+              "Looks like this we can't fetch data for this title"}
+          </div>
           <div className={styles.popularity}>
             <RatingLine rating={details.vote_average} />
             <PopularityLine popularity={details.popularity} />
