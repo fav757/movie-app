454c71481034579a93bf9b0b55b873bfee3c158c
fav757 <fav755@gmail.com>
2020-10-05 12:31:03 +0300

    add similar content for main pages

--- src/components/PostersGrid/PostersGrid.js
+++ src/components/PostersGrid/PostersGrid.js
@@ -6,20 +6,44 @@ import { useLocation } from 'react-router-dom';
 function PostersGrid() {
   const [trandingFilms, setTrandingFilms] = useState([]);
   const [postersStyle, setPostersStyle] = useState(styles.posters);
+  const [page, setPage] = useState(1);
   const location = useLocation();
+  console.log(location);
+
+  const changePage = (reduce) => {
+    setPage(() => {
+      if (reduce) {
+        return page + 1;
+      } else {
+        return page - 1 <= 0 ? 1 : page - 1;
+      }
+    });
+  };
 
   let requestLink, header;
   switch (location.pathname) {
     case '/home/tv-top': {
       header = 'Top rated tv series';
-      requestLink =
-        'https://api.themoviedb.org/3/tv/top_rated?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1';
+      requestLink = `https://api.themoviedb.org/3/tv/top_rated?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=${page}`;
+      break;
+    }
+    case '/tv': {
+      header = 'Similar tv series';
+      requestLink = `https://api.themoviedb.org/3/tv/${location.search.slice(
+        4
+      )}/similar?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=${page}`;
       break;
     }
     case '/home/movie-top': {
       header = 'Top rated movies';
-      requestLink =
-        'https://api.themoviedb.org/3/movie/top_rated?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=1';
+      requestLink = `https://api.themoviedb.org/3/movie/top_rated?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=${page}`;
+      break;
+    }
+    case '/movie': {
+      header = 'Similar movies';
+      requestLink = `https://api.themoviedb.org/3/movie/${location.search.slice(
+        4
+      )}/similar?api_key=09ecd60e9326551324881d2239a8f12a&language=en-US&page=${page}`;
       break;
     }
     case '/home/tranding':
@@ -60,13 +84,24 @@ function PostersGrid() {
         );
       }
     })();
-  }, [requestLink]);
+  }, [requestLink, page]);
 
   return (
     <section className={styles.container}>
       <div className={styles.tranding}>
         <h2>{header}</h2>
         <div className={postersStyle}>{trandingFilms}</div>
+        <div className={styles.pagesRow}>
+          <i
+            onClick={() => changePage(false)}
+            className='fas fa-arrow-circle-left'
+          ></i>
+          <span>{page}</span>
+          <i
+            onClick={() => changePage(true)}
+            className='fas fa-arrow-circle-right'
+          ></i>
+        </div>
       </div>
     </section>
   );
