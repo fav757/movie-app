d31d4d85586b1e8adff8ae40dd2e326807a862a8
fav757 <fav755@gmail.com>
2020-10-06 17:38:26 +0300

    create actions and add them to reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,3 +1,21 @@
+import { ADD_TO_LIST, REMOVE_FROM_LIST } from './rootActions';
+
 export default function (state = {}, action) {
-  return state;
+  switch (action.type) {
+    case ADD_TO_LIST: {
+      const newState = { ...state };
+      newState[action.payload.category].push(action.payload.id);
+      return newState;
+    }
+    case REMOVE_FROM_LIST: {
+      const newState = { ...state };
+      newState[action.payload.category].filter(
+        (item) => item !== action.payload.id
+      );
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
 }
