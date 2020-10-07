6b542b2cba254c6cc99fc6c0d0886e0f70dfc2df
fav757 <fav755@gmail.com>
2020-10-07 16:27:08 +0300

    add cross indicator for controll row component to display if film is already in list

--- src/components/ControlRow/ControlRow.js
+++ src/components/ControlRow/ControlRow.js
@@ -5,7 +5,10 @@ import styles from './ControlRow.module.scss';
 
 function ControlRow({ id }) {
   const { state, dispatch } = useContext(GlobalState);
-  console.log(state);
+
+  const isFavorite = !state.favorite.has(id);
+  const isWatched = !state.watched.has(id);
+  const isLater = !state.later.has(id);
 
   const handleClick = ({ target }) => {
     state[target.dataset.category].has(id)
@@ -15,21 +18,42 @@ function ControlRow({ id }) {
 
   return (
     <div className={styles.controls}>
-      <i
-        onClick={handleClick}
-        data-category='favorite'
-        className={styles.icon + ' fas fa-heart'}
-      ></i>
-      <i
-        onClick={handleClick}
-        data-category='watched'
-        className={styles.icon + ' fas fa-check'}
-      ></i>
-      <i
-        onClick={handleClick}
-        data-category='later'
-        className={styles.icon + ' fas fa-clock'}
-      ></i>
+      <div className={styles.wrap}>
+        <i
+          onClick={handleClick}
+          data-category='favorite'
+          className={styles.icon + ' fas fa-heart '}
+        ></i>
+        <i
+          className={
+            styles.cross + ' fas fa-times ' + (isFavorite && styles.hidden)
+          }
+        ></i>
+      </div>
+      <div className={styles.wrap}>
+        <i
+          onClick={handleClick}
+          data-category='watched'
+          className={styles.icon + ' fas fa-check'}
+        ></i>
+        <i
+          className={
+            styles.cross + ' fas fa-times ' + (isWatched && styles.hidden)
+          }
+        ></i>
+      </div>
+      <div className={styles.wrap}>
+        <i
+          onClick={handleClick}
+          data-category='later'
+          className={styles.icon + ' fas fa-clock'}
+        ></i>
+        <i
+          className={
+            styles.cross + ' fas fa-times ' + (isLater && styles.hidden)
+          }
+        ></i>
+      </div>
     </div>
   );
 }
