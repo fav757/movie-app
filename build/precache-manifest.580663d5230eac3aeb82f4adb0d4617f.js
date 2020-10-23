a57e2a378cc511715ab0873523dc5ea7966f6389
fav757 <fav755@gmail.com>
2020-10-23 14:27:26 +0300

    create build version

---
+++ build/precache-manifest.580663d5230eac3aeb82f4adb0d4617f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996575c8029065fe133405f2c692196b",
+    "url": "/movie-app/build/index.html"
+  },
+  {
+    "revision": "29b29216dae06a5fa9d3",
+    "url": "/movie-app/build/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c8742484a37ed2a372f4",
+    "url": "/movie-app/build/static/css/main.d5d17321.chunk.css"
+  },
+  {
+    "revision": "29b29216dae06a5fa9d3",
+    "url": "/movie-app/build/static/js/2.c44006c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/build/static/js/2.c44006c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8742484a37ed2a372f4",
+    "url": "/movie-app/build/static/js/main.4e5a94ac.chunk.js"
+  },
+  {
+    "revision": "f03ea7ab7860e06792f9",
+    "url": "/movie-app/build/static/js/runtime-main.51dac4e0.js"
+  },
+  {
+    "revision": "4d15c0ae95815b2e47e6f374b622a94a",
+    "url": "/movie-app/build/static/media/backgroundFone.4d15c0ae.jpeg"
+  }
+]);
