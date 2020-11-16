cfcce7f8e2bbc1981cfaec8ca67ca79332fe441e
fav757 <fav755@gmail.com>
2020-11-16 14:34:34 +0200

    migrarte RatingLine component to TypeScript

--- src/components/RatingLine/RatingLine.tsx
+++ src/components/RatingLine/RatingLine.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './RatingLine.module.scss';
 
-type RatingLine = {
+type RatingLineType = {
   rating: number;
 };
 
-const RatingLine: React.FC<RatingLine> = ({ rating }) => {
-  const smileIcon = rating > 6.66 ? 'smile' : rating > 3.33 ? 'meh' : 'frown';
+const RatingLine: React.FC<RatingLineType> = ({ rating }) => {
+  let smileIcon: string;
+
+  if (rating > 6.66) {
+    smileIcon = 'smile';
+  } else if (rating > 3.33) {
+    smileIcon = 'meh';
+  } else {
+    smileIcon = 'frown';
+  }
 
   return (
-    <div className={styles.container + ' ' + smileIcon}>
-      <i className={'far fa-' + smileIcon}></i>
+    <div className={`${styles.container} ${smileIcon}`}>
+      <i className={`far fa- ${smileIcon}`} />
       <span> {rating}</span>
     </div>
   );
