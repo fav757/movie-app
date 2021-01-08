2087c41085095cd5fed6352bfe7d829a664165e5
meskill <8974488+meskill@users.noreply.github.com>
2021-01-08 13:46:06 +0300

    Update src/serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -33,7 +33,7 @@ export function register(config) {
       return;
     }
 
-    window.addEventListener('load', () => {
+    window.addEventListen('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
